2105052d25f76c7fae6cfa0755424f46e6965b8f
rusikcrasava <rusikcrasava@mail.ru>
2022-12-26 17:34:19 +0300

    Первая задача сделана

---
+++ 1zada4a/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+    Console.Write("Напишите далее трехзначное число: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write($"Вторая цифра вашего числа: {(number/10)%10}");
