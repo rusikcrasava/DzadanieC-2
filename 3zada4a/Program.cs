a252d2c87ce92e16230321e962a0b2d842f3e850
rusikcrasava <rusikcrasava@mail.ru>
2022-12-26 17:46:41 +0300

    Третья задача выполнена

---
+++ 3zada4a/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+    Console.Write("Введите цифрой день недели: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num<1 || num>7)
+    {
+      Console.Write("Не существует такого дня недели");
+    }
+    else
+    {
+    if(6<=num && num <=7)
+      {
+    Console.Write("Да, это выходной, Уррррааа!");      
+      }
+      else
+      {
+    Console.Write("К сожалению, но отдохнуть не получиться :(");          
+      }
+    }
